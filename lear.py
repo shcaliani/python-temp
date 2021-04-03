9d79f1d5e192dad51ac1571e03bda7cde68ee34d
shcaliani <shcaliani@gmail.com>
2021-04-03 18:18:21 -0300

    sobe branch teste alt 4

--- lear.py
+++ lear.py
@@ -6,9 +6,11 @@ print ('----')
 
 print ('alterado na branch teste 1')
 
-print('mais um')
+print('mais um teste na branch teste')
 print("----")
 
 print('alterado na branch teste 2')
+
+print('alterado na branch teste 4')
 
 ## final 
